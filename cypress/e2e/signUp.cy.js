fecfad6a51c0602a1647e327071e420980ab32cc
Oleksandr Kovalchuk <kov280396@gmail.com>
2025-06-01 17:48:34 +0300

    tests for sign in logic

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -1,5 +1,19 @@
+const { generateUser } = require('../support/generate');
+
 describe('Sign Up page', () => {
   it('should provide an ability to register new account', () => {
+    const user = generateUser();
 
+    cy.visit('/');
+
+    cy.get('a[href="/user/register"]').contains('Sign up').click();
+
+    cy.get('input[placeholder*="Username"]').type(user.username);
+    cy.get('input[placeholder*="Email"]').type(user.email);
+    cy.get('input[placeholder*="Password"]').type(user.password);
+
+    cy.get('button[type="submit"]').contains('Sign up').click();
+
+    cy.get('.navbar').should('contain.text', user.username.toLowerCase());
   });
 });
